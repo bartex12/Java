fb6f114c414e73561a4cf1125bfc22dcbc1b1860
Андрей <barcats@inbox.ru>
2019-07-26 15:31:16 +0300

    сделал подсказки

--- scr/JFrame/My_Faq.java
+++ scr/JFrame/My_Faq.java
@@ -1,4 +1,26 @@
 package JFrame;
 
-public class My_Faq {
+import javax.swing.*;
+import java.awt.*;
+
+public class My_Faq extends JFrame{
+    public My_Faq(){
+        setTitle("FAQ");
+        setBounds(450,200,400,400);
+        setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        JTextArea jta_faq = new JTextArea();
+        jta_faq.setBackground(new Color( 230,250,245));
+        jta_faq.setText("\n\t1 Что стоит между окном и дверью? \n\t(Буква “и”) \n\n" +
+                "\t2 Чем оканчиваются день и ночь? \n\t(Мягким знаком)\n\n" +
+                "\t3 Что такое кульбит?\n\t(кулёк с битами)\n\n" +
+                "\t4 Какой компьютерный термин английского\n\t происхождения при дословном переводе \n" +
+                "\tозначает междумордие»? \n\t(интерфейс)\n\n" +
+                "\t https://www.anekdotovmir.ru\n" +
+                "\thttp://kat-lv.ru/?page_id=119");
+        //jta_faq.setBounds();
+        jta_faq.setEditable(false);
+        JScrollPane jsp = new JScrollPane(jta_faq);
+        add(jsp, BorderLayout.CENTER);
+        setVisible(true);
+    }
 }
--- scr/JFrame/My_Help.java
+++ scr/JFrame/My_Help.java
@@ -1,4 +1,20 @@
 package JFrame;
 
-public class My_Help {
+import javax.swing.*;
+
+public class My_Help extends JFrame {
+    public My_Help(){
+        setTitle("About");
+        setBounds(450,200,400,120);
+        setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        //можно указать абсолютный путь к картинку на компьютере
+        //ImageIcon imageIcon = new ImageIcon("D:\\Java_Programs\\GeekBrains\\smile.png");
+        //но лучше - указать путь к картинке  в проекте
+        ImageIcon imageIcon = new ImageIcon("scr/smile.png");
+        //System.out.println(imageIcon.getIconWidth());
+        JLabel mLabel1 = new JLabel("  Программма сделана на расслабоне.", JLabel.CENTER);
+        mLabel1.setIcon(imageIcon);
+        add(mLabel1);
+        setVisible(true);
+    }
 }
