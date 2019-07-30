1d160a4c85f0b53f63ab08c5a268bd50565ee976
Андрей <barcats@inbox.ru>
2019-07-30 21:24:43 +0300

    причесал код

--- scr/Lesson_5/ArrayChange.java
+++ scr/Lesson_5/ArrayChange.java
@@ -4,25 +4,25 @@ class ArrayChange_Demo{
     public static void main(String[] args) {
 
         final int numberRepeat = 5000000;
-        //final int numberThreads = 5;
 
         ArrayChange demo = new ArrayChange(numberRepeat);
-        demo.arrayChangeOneThread();
+        demo.arrayChangeOneThread();  //метод для 1 потока
 
         System.out.println();
         System.out.println("**************************************");
 
         ArrayChange demo1 = new ArrayChange(numberRepeat, 2);
-        demo1.arrayChangeManyThreads();
+        demo1.arrayChangeManyThreads();  //метод для нескольких потоков (сейчас - 2)
 
         System.out.println();
         System.out.println("**************************************");
 
         ArrayChange demo2 = new ArrayChange(numberRepeat, 5);
-        demo2.arrayChangeManyThreads();
+        demo2.arrayChangeManyThreads(); //метод для нескольких потоков (сейчас - 5)
     }
 }
 
+//*******************************************************************
 
 public class ArrayChange {
     int size;
@@ -30,6 +30,7 @@ public class ArrayChange {
     float[] arr;
     int numberParts;
 
+    //конструктор для нескольких потоков
     public ArrayChange(int size, int numberParts) {
         this.size = size;
         if (numberParts<2){
@@ -46,6 +47,7 @@ public class ArrayChange {
         System.out.println();
     }
 
+    //конструктор для 1 потока
     public ArrayChange(int size) {
         this.size = size;
         numberParts = 2;
@@ -58,6 +60,7 @@ public class ArrayChange {
     }
 
     //***************************************************************
+
     // Метод расчёта в одном потоке
     void arrayChangeOneThread() {
         long a = System.currentTimeMillis();  // Время начала работы
@@ -70,9 +73,10 @@ public class ArrayChange {
         System.out.println("Один поток. Общее время работы = = " +
                 (System.currentTimeMillis() - a) + " миллисекунд");
     }
+
     //**************************************************************************
 
-
+    //синхронизированный метод пересчёта элементов массива
     public synchronized float[] arrayChange(float[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = (float)(array[i] * Math.sin(0.2f + i / 5) *
@@ -80,6 +84,8 @@ public class ArrayChange {
         }
         return array;
     }
+
+    //**************************************************************************
 
     // Метод расчёта в нескольких потоках
     void arrayChangeManyThreads() {
@@ -109,7 +115,6 @@ public class ArrayChange {
                     long lapBeforeChangingArray = System.currentTimeMillis(); //начало обработки в потоке
                     if (finalI1 <array.length){
                         arrayChange(arr1);
-                        long lapAfterChangingArray = System.currentTimeMillis() - lapDivide-start;
                         System.out.println( "Количество потоков - " + numberParts + "   Время изменения  массива в очередном потоке = " +
                                 (System.currentTimeMillis() - lapBeforeChangingArray) + " миллисекунд");
                     }else {
@@ -139,81 +144,12 @@ public class ArrayChange {
         System.arraycopy(arr1, 0, arr, 0, half);
         System.arraycopy(arr2, 0, arr, half, half);
 
-        System.out.println("Количество потоков - " + numberParts +  " Время объединения  массива = " +
+        System.out.println("Количество потоков - " + numberParts +  " Время объединения  массивов = " +
                 (System.currentTimeMillis() - arrayBeforeJoint) + " миллисекунд");
         System.out.println("----------------------------");
         System.out.println("Количество потоков - " + numberParts + " Общее время работы = " +
                 (System.currentTimeMillis() - start) + " миллисекунд");
     }
-
-
-//    // Метод расчёта в двух потоках
-//    void arrayChangeTwoThreads() {
-//
-//        long start = System.currentTimeMillis(); //начало работы метода на 2 потока
-//        //Разбивка на 2 массива равной длины
-//        float[] arr1 = new float[half];
-//        float[] arr2 = new float[half];
-//        System.arraycopy(arr, 0, arr1, 0, half);
-//        System.arraycopy(arr, half, arr2, 0, half);
-//
-//        System.out.println("Количество потоков - 2. Время разбивки массива на 2 части = " +
-//                (System.currentTimeMillis() - start) + " миллисекунд");
-//        System.out.println();
-//        System.out.println("-----------------------------");
-//
-//
-//        //формирование потока 1
-//        float[] finalArr1 = arr1;
-//        Thread t1 = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                long arrayBeforeChanging = System.currentTimeMillis(); //начало обработки в потоке
-//
-//                arrayChange(finalArr1);
-//
-//                System.out.println( "Количество потоков - 2. Время изменения  массива в потоке 1 = " +
-//                        (System.currentTimeMillis() - arrayBeforeChanging) + " миллисекунд");
-//            }
-//        });
-//
-//        //формирование потока 2
-//        float[] finalArr2 = arr2;
-//        Thread t2 = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                long arrayBeforeChanging = System.currentTimeMillis(); //начало обработки в потоке
-//
-//                arrayChange(finalArr2);
-//
-//                System.out.println( "Количество потоков - 2. Время изменения  массива в потоке 2 = " +
-//                        (System.currentTimeMillis() - arrayBeforeChanging) + " миллисекунд");
-//            }
-//        });
-//
-//        t1.start();
-//        t2.start();
-//
-//        try {
-//            t1.join();
-//            t2.join();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println("----------------------------");
-//
-//        long arrayBeforeJoint = System.currentTimeMillis(); //время начала объединения
-//        System.arraycopy(arr1, 0, arr, 0, half);
-//        System.arraycopy(arr2, 0, arr, half, half);
-//
-//        System.out.println("Количество потоков - 2. Время объединения  массива = " +
-//                (System.currentTimeMillis() - arrayBeforeJoint) + " миллисекунд");
-//        System.out.println("----------------------------");
-//        System.out.println("Количество потоков - 2. Общее время работы = " +
-//                (System.currentTimeMillis() - start) + " миллисекунд");
-//    }
-
 }
 
 
