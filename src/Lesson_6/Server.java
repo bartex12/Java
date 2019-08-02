da5a85ec0093ea4334802c8c473aa9bb592aae0c
Андрей <barcats@inbox.ru>
2019-08-02 21:41:37 +0300

    двусторонняя связь сервер - putty

--- src/Lesson_6/Server.java
+++ src/Lesson_6/Server.java
@@ -1,6 +1,7 @@
 package Lesson_6;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -18,11 +19,13 @@ public class Server {
             System.out.println("Клиент подключился");
 
             Scanner sc = new Scanner(socket.getInputStream());
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
 
             while (true){
                 String str = sc.nextLine();
                 System.out.println("Client - " + str);
 
+                writer.println("Echo - " + str); ///!!!writer.println а не System.out.println (ИНАЧЕ ВЫВОД В КОНСОЛЬ)
             }
         } catch (IOException e) {
             e.printStackTrace();
