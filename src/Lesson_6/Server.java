e8bc3924aaf28cce17ad0748d35b0370d5f03e48
Андрей <barcats@inbox.ru>
2019-08-02 21:30:21 +0300

    минимальная программаБ которая состоит из сервера и клиента в виде программы putty

--- src/Lesson_6/Server.java
+++ src/Lesson_6/Server.java
@@ -1,4 +1,44 @@
 package Lesson_6;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен");
+
+            socket = server.accept();
+            System.out.println("Клиент подключился");
+
+            Scanner sc = new Scanner(socket.getInputStream());
+
+            while (true){
+                String str = sc.nextLine();
+                System.out.println("Client - " + str);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
