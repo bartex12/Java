3ad2e26015b15ff79889ea79ddc24e6dad934245
Андрей <barcats@inbox.ru>
2019-08-28 15:26:07 +0300

    ==

--- src/Less3_Additional/SequenceFiles_Demo.java
+++ src/Less3_Additional/SequenceFiles_Demo.java
@@ -4,14 +4,17 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class SequenceFiles {
-    private static void seqInputStreamWork() throws IOException {
+public class SequenceFiles_Demo  {
+
+    public static void main(String[] args) throws IOException{
         ArrayList<InputStream> arr = new ArrayList<>();
-        arr.add(new FileInputStream("123/test1.txt"));
-        arr.add(new FileInputStream("123/test2.txt"));
-        arr.add(new FileInputStream("123/test3.txt"));
-        arr.add(new FileInputStream("123/test4.txt"));
-        arr.add(new FileInputStream("123/test5.txt"));
+
+        arr.add(new FileInputStream("123/1.txt"));
+        arr.add(new FileInputStream("123/2.txt"));
+        arr.add(new FileInputStream("123/3.txt"));
+        arr.add(new FileInputStream("123/4.txt"));
+        arr.add(new FileInputStream("123/5.txt"));
+
         SequenceInputStream seq = new SequenceInputStream(Collections.enumeration(arr));
         int x;
         while ((x=seq.read())!=-1){
