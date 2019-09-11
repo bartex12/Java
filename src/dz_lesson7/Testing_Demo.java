e172d4c0300af6ad0dd46fc7c7f2988c0f4bd3a0
Андрей <barcats@inbox.ru>
2019-09-11 16:12:52 +0300

    добавил выброс исключения  если after - более одного ДОБАВИЛ КОММЕНТЫ

--- src/dz_lesson7/Testing_Demo.java
+++ src/dz_lesson7/Testing_Demo.java
@@ -18,31 +18,52 @@ public class Testing_Demo {
 
     public static void  start(Class testClass){
 
+        // 1- перебираем методы класса, запоминаем методы с  аннотацией beforeSuite и afterSuite,
+        // формируем список методов с аннотацией Testing
+        // 2-Запускаем метод с аннотацией beforeSuite
+        // 3-сортируем тестовые методы по параметрам аннотаций
+        // 4-запускаем тестовые методы по порядку сортировки
+        // 5-Запускаем метод с аннотацией afterSuite
+
         ArrayList<Method> arr = new ArrayList<>();
         int beforNumber = -1;  //считаем методы с аннотацией beforeSuite
+        int afterNumber = -1;  //считаем методы с аннотацией afterSuite
 
         //получаем методы класса, включая методы суперкласса
         Method[] methods = testClass.getMethods();
+        Method befor = null;
+        Method after = null;
+
         //перебираем методы
         for (Method m: methods){
-            //если аннотация метода ==beforeSuite
+            //если аннотация метода ==beforeSuite, считаем их количество
             if (m.getAnnotation(beforeSuite.class)!=null){
-                try {
-                    //запускаем данный метод
-                    m.invoke(testClass.getConstructor().newInstance());
+                befor =m;
                     beforNumber++;
-                } catch (InstantiationException | IllegalAccessException |
-                        NoSuchMethodException | InvocationTargetException e) {
-                    e.printStackTrace();
-                }
             }
             //если аннотация Testing, добавляем в список методов
             if (m.getAnnotation(Testing.class)!=null){
                 arr.add(m);
             }
+            //если аннотация afterSuite, считаем их количество
+            if (m.getAnnotation(afterSuite.class)!=null){
+                after = m;
+                afterNumber++;
+            }
         }
         if (beforNumber>0){
             throw new RuntimeException(" Более одного метода с аннотацией @beforeSuite");
+        }
+        if (afterNumber>0){
+            throw new RuntimeException(" Более одного метода с аннотацией @afterSuite");
+        }
+
+        // Запускаем метод с аннотацией beforeSuite
+        try {
+            befor.invoke(testClass.getConstructor().newInstance());
+        } catch (InstantiationException | IllegalAccessException |
+                NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
         }
 
         //сортируем тестовые методы по параметрам аннотаций
@@ -66,20 +87,12 @@ public class Testing_Demo {
             }
         }
 
-        // //если аннотация метода ==afterSuite
-        methods = testClass.getMethods();
-        for (Method m: methods){
-            //если аннотация метода ==afterSuite
-            if (m.getAnnotation(afterSuite.class)!=null){
-                //System.out.println("!=null ");
-                try {
-                    //запускаем данный метод
-                    m.invoke(testClass.getConstructor().newInstance());
-                } catch (InstantiationException | IllegalAccessException |
-                        NoSuchMethodException | InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-            }
+        // Запускаем метод с аннотацией afterSuite
+        try {
+            after.invoke(testClass.getConstructor().newInstance());
+        } catch (InstantiationException | IllegalAccessException |
+                NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
         }
     }
 }
